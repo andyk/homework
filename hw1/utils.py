7b89bf3885115f860e5138a798f51a6e5cb33079
Andy Konwinski <andykonwinski@gmail.com>
2019-08-08 12:44:22 -0700

    adding utils for str2bool

---
+++ hw1/utils.py
@@ -0,0 +1,9 @@
+def str2bool(arg):
+    if isinstance(arg, bool):
+        return arg
+    if arg.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif arg.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
